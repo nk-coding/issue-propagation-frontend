9b3879cd1625a294637347234475bb4662860d00
nk-coding <niklask.coding@gmail.com>
2023-11-06 11:07:35 +0100

    fix padding

--- src/components/GropiusCard.vue
+++ src/components/GropiusCard.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-card color="surface-elevated-2" rounded="xl" elevation="0" class="pa-4">
-        <div class="d-flex align-center justify-center">
+    <v-card color="surface-elevated-2" rounded="xl" elevation="0" class="px-7 py-3">
+        <div class="d-flex align-center justify-center pt-2">
             <img src="@/assets/logo.svg" width="50" /><span class="text-primary text-h4 ml-2">Gropius</span>
         </div>
         <slot />
